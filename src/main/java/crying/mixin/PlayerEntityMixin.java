b10539c3933286ab9f9080c91f7896369ad0bc8c
Thomas Darkson <152335332+ThomasDarkson@users.noreply.github.com>
2025-11-09 17:19:38 +0300

    6.1.4

--- src/main/java/crying/mixin/PlayerEntityMixin.java
+++ src/main/java/crying/mixin/PlayerEntityMixin.java
@@ -13,7 +13,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.tag.DamageTypeTags;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.storage.ReadView;
 import net.minecraft.storage.WriteView;
@@ -97,14 +96,6 @@ public class PlayerEntityMixin implements SanityVars, HookVars, FoodVars, BiomeV
                 if (!player.isCreative() && source.getAttacker() instanceof Entity)
                     GranterEntity.summonGranterEntity(world, player);
             }
-        }
-    }
-
-    @Inject(method = "tick", at = @At("TAIL"))
-    public void tick(CallbackInfo info) {
-        var player = (PlayerEntity) (Object) this;
-        if (player instanceof ServerPlayerEntity serverPlayer && Crying.getSanityManager(serverPlayer).getMaxLevel() > 0) {
-            this.SanityManager.update(serverPlayer);
         }
     }
 
